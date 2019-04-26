6632f3b71f92d1c133528604fbba480f501ea534
Janice Pena <jipena94@gmail.com>
2019-04-26 04:11:00 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,16 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(hash)
-  hash.length
+  counter = 0
+  while counter < array.length
+  if array[counter].even?
+  array[counter] = "redacted"
+end
+counter = counter + 1
+end
+
+array
+end
+a = [1,2,3,4]
+replace_even(a)
 end
