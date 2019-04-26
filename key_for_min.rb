ee63f4686d40018fb4f1fa4f50d90a8d046df9e9
Janice Pena <jipena94@gmail.com>
2019-04-26 04:17:49 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,7 +3,7 @@
 
 def key_for_min_value(name_hash)
   min_value = 0 
-  min_key = 1
+  min_key = nil
   name_hash.each do |key, value|
     if min_value == 0 || value < min_value
       min_value = value
