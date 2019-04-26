9558090c89bb8a6f7e1fe724ef4c820100c64f0d
Janice Pena <jipena94@gmail.com>
2019-04-26 04:12:59 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,13 +1,5 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-def key_for_min_value(hash)
-  counter = 0
-  while counter < hash.length
-  if hash[counter].even?
-  array[counter] = "redacted"
-end
-counter = counter + 1
-end
-array
+def key_for_min_value(name_hash)
 end
