62c496739b4d983cb9ab7fec913ee9c6a0e11a81
Janice Pena <jipena94@gmail.com>
2019-04-26 04:11:50 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,15 +3,11 @@
 
 def key_for_min_value(hash)
   counter = 0
-  while counter < array.length
-  if array[counter].even?
+  while counter < hash.length
+  if hash[counter].even?
   array[counter] = "redacted"
 end
 counter = counter + 1
 end
-
 array
-end
-a = [1,2,3,4]
-replace_even(a)
 end
